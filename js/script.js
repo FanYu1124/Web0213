80ce8d4b916fb86022373f15e243f6991884c623
FanYu1124 <99062266+FanYu1124@users.noreply.github.com>
2025-02-13 19:59:16 +0800

    v1.0

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+// 按下按鈕之後產生的變化
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+// 回到上層按鈕
+window.onscroll = () => {
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-up').classList.add('active');
+
+    } else {
+        document.querySelector('#scroll-up').classList.remove('active');
+    }
+}
+
+const scriptURL='https://script.google.com/macros/s/AKfycbzYMq0fsjWd4CpXFfzfLYddcZTVw73Gdd36PMv3xSm6kyCK2_om3CvOqMaWdq5rok-QNA/exec';
+const form = document.forms['orderlist'];
+	  
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+	  .then(response => alert("You have successfully submitted."))
+	  .catch(error => console.error('Error!', error.message))
+})
